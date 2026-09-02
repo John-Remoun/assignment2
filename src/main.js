e870e6bbf002d69fa79ad1aa965b4b55815851d0
John-Remoun <johnremoun789@gmail.com>
2026-09-02 16:47:04 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import app from "./app.js";
 import { connectDatabase } from "./database/database.service.js";
 import { PORT } from "./config/config.service.js";
+import dns from "node:dns";
+
+// Fix MongoDB Atlas SRV DNS resolution
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 async function bootstrap() {
-    // 1. Connect Database
-    await connectDatabase();
+  // 1. Connect Database
+  await connectDatabase();
 
-    // 2. Start HTTP Server
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT} 🚀😉`);
-    });
+  // 2. Start HTTP Server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} 🚀😉`);
+  });
 }
 
 bootstrap();
